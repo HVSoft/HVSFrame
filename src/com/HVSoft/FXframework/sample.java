7d8a005852817af53087f78eb96622a99552d4dc
HVSig <dev.harsha99@gmail.com>
2018-02-13 00:37:01 +0530

    Test commit

---
+++ src/com/HVSoft/FXframework/sample.java
@@ -0,0 +1,27 @@
+package com.HVSoft.FXframework;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class sample extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	      Group group = new Group();  
+	      Scene scene = new Scene(group ,1280, 720);
+	      scene.setFill(Color.ALICEBLUE);
+	      primaryStage.setTitle("Test");
+	      primaryStage.setScene(scene);
+	      primaryStage.show();
+	      
+	}
+
+}
